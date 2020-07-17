5d9abb72d397cd07da604f39c192c0f925db8651
Saymur <nls998@gmail.com>
2020-07-17 23:24:28 +0400

    all global variables have been moved to the main

--- Task_2/map_tasks/map5_8tasks/map58.go
+++ Task_2/map_tasks/map5_8tasks/map58.go
@@ -14,34 +14,8 @@ type StorageIds map[string]uint // [товар]id
 type Orders map[string]float64
 type User map[string]float64 // [имя]счёт
 
-// товары-цена
-var storeBase = Storage{
-	"lamp":      2.35,
-	"keyboard":  5.25,
-	"phone":     152.0,
-	"usb_cable": 3.0,
-}
-
-// товары Id
-var storeBaseIds = StorageIds{
-	"lamp":      0,
-	"keyboard":  1,
-	"phone":     2,
-	"usb_cable": 3,
-}
-
-// заказы
-var ordersBase = make(Orders)
-
-// баланс пользователей
-var usersBase = User{
-	"Admin":    454.00,
-	"User":     55.45,
-	"RichUser": 1150,
-}
-
 // получить сумму заказа
-func (crt Cart) GetTotal() float64 {
+func (crt Cart) GetTotal(storeBase Storage) float64 {
 	total := 0.0
 	for name, number := range crt {
 		total += storeBase[name] * float64(number)
@@ -50,7 +24,7 @@ func (crt Cart) GetTotal() float64 {
 }
 
 // добавить товар
-func (strg Storage) AppendPr(product string, cost float64) bool {
+func (strg Storage) AppendPr(storeBase Storage, storeBaseIds StorageIds, product string, cost float64) bool {
 	if product == "" || cost <= 0.0 {
 		return false
 	}
@@ -84,7 +58,7 @@ func (strg Storage) UpdatePr(product string, cost float64) bool {
 
 // получить из map заказа стоку вида "|0:2|1:5|2:5..."
 // где |id товара:кол-во_товара
-func (crt Cart) MakeSrtByCart(sortedKeys *[]string) string {
+func (crt Cart) MakeSrtByCart(storeBaseIds StorageIds, sortedKeys *[]string) string {
 	b := new(bytes.Buffer)
 	for _, val := range *sortedKeys {
 		fmt.Fprintf(b, "|%d:%d", storeBaseIds[val], crt[val])
@@ -109,13 +83,13 @@ func (crt Cart) GetSotedKeys() []string {
 если такого ключа нет то сумма считается при помощи GetTotal()
 из записывается в ordersBase, ключом является - полученная ранее строка
 */
-func (crt Cart) GetTotalMulti() float64 {
+func (crt Cart) GetTotalMulti(storeBase Storage, storeBaseIds StorageIds, ordersBase Orders) float64 {
 	var total float64
 	// получить срез отсортированных ключей
 	sortedKeys := crt.GetSotedKeys()
 	// на основе упорядоченной карты
 	// создать ключ-строку
-	crtStr := crt.MakeSrtByCart(&sortedKeys)
+	crtStr := crt.MakeSrtByCart(storeBaseIds, &sortedKeys)
 
 	// проверить наличие значения для ключ-строки в ordersBase
 	if val, ok := ordersBase[crtStr]; ok {
@@ -125,7 +99,7 @@ func (crt Cart) GetTotalMulti() float64 {
 	} else {
 		// если строка не найдена то посчитать сумму заказа
 		fmt.Println("No such order in base - counting total...")
-		total = crt.GetTotal()
+		total = crt.GetTotal(storeBase)
 		ordersBase[crtStr] = total
 	}
 	return total
@@ -134,8 +108,8 @@ func (crt Cart) GetTotalMulti() float64 {
 // считается сумма заказа
 // и списывается со счёта если
 // у пользователя достаточно средств
-func (crt Cart) IsAllowedFor(user string) bool {
-	total := crt.GetTotalMulti()
+func (crt Cart) IsAllowedFor(storeBase Storage, storeBaseIds StorageIds, ordersBase Orders, usersBase User, user string) bool {
+	total := crt.GetTotalMulti(storeBase, storeBaseIds, ordersBase)
 	if balance := usersBase[user]; balance < total {
 		return false
 	}
@@ -192,23 +166,51 @@ func (usr User) PrByBalance() {
 }
 
 func main() {
+
+	// переменные для реализации магазина
+	// товары-цена
+	var storeBase = Storage{
+		"lamp":      2.35,
+		"keyboard":  5.25,
+		"phone":     152.0,
+		"usb_cable": 3.0,
+	}
+
+	// товары Id
+	var storeBaseIds = StorageIds{
+		"lamp":      0,
+		"keyboard":  1,
+		"phone":     2,
+		"usb_cable": 3,
+	}
+
+	// заказы
+	var ordersBase = make(Orders)
+
+	// баланс пользователей
+	var usersBase = User{
+		"Admin":    454.00,
+		"User":     55.45,
+		"RichUser": 1150,
+	}
+
 	fmt.Println("5. Пользователь даёт список заказа, программа должна по map с наименованиями товаров и ценами, посчитать сумму заказа. И сделать метод добавления новых товаров в map, и метод обновления цены уже существующего товара")
 	crt := Cart{"keyboard": 1, "phone": 1, "lamp": 2}
-	fmt.Println(crt.GetTotal())
+	fmt.Println(crt.GetTotal(storeBase))
 	fmt.Println("Добавим новый товар:")
-	storeBase.AppendPr("microphone", 3.25)
+	storeBase.AppendPr(storeBase, storeBaseIds, "microphone", 3.25)
 	fmt.Println(storeBase)
 	fmt.Println("Обновим его цену:")
 	storeBase.UpdatePr("microphone", 999.0)
 	fmt.Println(storeBase)
 
 	fmt.Println("6. Сделать 1е, но у нас приходит несколько сотен таких списков заказов и мы хотим запоминать уже посчитанные заказы, чтобы если встречается такой же, то сразу говорить его цену без расчёта")
-	fmt.Println(crt.GetTotalMulti())
-	fmt.Println(crt.GetTotalMulti())
+	fmt.Println(crt.GetTotalMulti(storeBase, storeBaseIds, ordersBase))
+	fmt.Println(crt.GetTotalMulti(storeBase, storeBaseIds, ordersBase))
 	fmt.Println("7. К 2 добавить, чтобы хранились пользовательские аккаунты со счетом типа \"вася: 300р, петя: 30000000р\". И перед оформлением заказа, но после его расчёта мы проверяли, а есть ли деньги у пользователя, и если есть, то списывали сумму заказа.")
 	user := "Admin"
 	fmt.Println("Счёт пользователя", usersBase[user])
-	fmt.Println("Достаточно ли средств:", crt.IsAllowedFor(user))
+	fmt.Println("Достаточно ли средств:", crt.IsAllowedFor(storeBase, storeBaseIds, ordersBase, usersBase, user))
 	fmt.Println("Счёт пользователя после сделки", usersBase[user])
 
 	fmt.Println("8. Есть map аккаунтов и счетов, как описано в 3. Надо вывести ее в отсортированном виде с сортировкой: по имени в алфавитном порядке, по имени в обратном порядке, по количеству денег по убыванию\n")
